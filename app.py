1c4ab95210df1becc5a8f6c325ddfd0854c61bc9
20210647-Alexander <20210647@uthh.edu.mx>
2025-06-18 23:00:42 -0600

    modelo new

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
 import logging
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,26 +10,37 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('model.pkl')
+model = joblib.load('modelo.pkl')
 app.logger.debug('Modelo cargado correctamente.')
+
+# Definir media y desviación estándar basadas en el conjunto de entrenamiento
+mean_values = np.array([4.4, 5.17])  # Media de [abdomen, antena]
+std_values = np.array([3.05, 2.65])  # Desviación estándar de [abdomen, antena]
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+    """Renderiza la plantilla del formulario."""
+    return render_template('formulario.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """Realiza una predicción basada en los datos del formulario."""
     try:
         # Obtener los datos enviados en el request
         abdomen = float(request.form['abdomen'])
-        antena = float(request.form['antena'])
+        antenna = float(request.form['antenna'])
 
         # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        data_df = pd.DataFrame([[abdomen, antenna]], columns=['abdomen', 'antenna'])
         app.logger.debug(f'DataFrame creado: {data_df}')
 
+        # Normalizar los datos manualmente (usando media y desviación estándar del entrenamiento)
+        data_array = data_df.values
+        data_scaled = (data_array - mean_values) / std_values
+        app.logger.debug(f'Datos normalizados: {data_scaled}')
+
         # Realizar predicciones
-        prediction = model.predict(data_df)
+        prediction = model.predict(data_scaled)
         app.logger.debug(f'Predicción: {prediction[0]}')
 
         # Devolver las predicciones como respuesta JSON
